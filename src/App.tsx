4175324c90002a20209d5f148bb1405ab492af8a
Tim Auf dem Kampe <tim.aufdemkampe@gmx.de>
2022-11-08 11:27:58 +0100

    Setup Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { LanguageSelectView } from './views/LanguageSelect/LanguageSelectView'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<LanguageSelectView />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
+  )
 }
 
-export default App;
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  padding: 50px;
+`
+
+export default App
